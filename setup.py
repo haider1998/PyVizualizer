6c9a42e0c351d8fa804e9aa980d3183cf88e0098
Syed Mohd Haider Rizvi <srizvi13@ford.com>
2025-03-23 02:53:12 +0530

    Project Public Release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="pyvisualizer",
+    name="py-code-visualizer",
     version="1.0.0",
     author="Syed Mohd Haider Rizvi",
     author_email="smhrizvi281@gmail.com",
